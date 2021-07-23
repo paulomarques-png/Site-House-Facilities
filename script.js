bda36bf66adcbb6717d81d2acb0e8385e30d8765
Paulo Marques <pauloalmarques@hotmail.com>
2021-07-23 11:50:04 -0300

    Estilo Finalizado Novo Menu Responsivo

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const btnMobile = document.getElementById('btn-mobile');
-
-function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-  }
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('open');
 }
 
+function zero() {
+  const btnmobile = document.getElementById('btn-mobile');
+  btnmobile.classList.toggle('active');
+}
+
+function corpo() {
+  const corpo = document.getElementById('corpo');
+  corpo.classList.toggle('over');
+}
+btnMobile.addEventListener('click', corpo);
 btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener('click', zero);
